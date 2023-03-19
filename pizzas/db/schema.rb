1c682c56d3db36e055cb3a99cd8dd003b95d6130
Jeff <jeffgichuki68@gmail.com>
2023-03-19 22:10:38 +0300

    completed

--- pizzas/db/schema.rb
+++ pizzas/db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_19_175304) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_19_183215) do
   create_table "pizzas", force: :cascade do |t|
     t.string "name"
+    t.string "ingredients"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "restaurant_pizzas", force: :cascade do |t|
-    t.integer "price"
+    t.decimal "price"
     t.integer "restaurant_id", null: false
     t.integer "pizza_id", null: false
     t.datetime "created_at", null: false
@@ -29,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_19_175304) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
+    t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- pizzas/db/seeds.rb
+++ pizzas/db/seeds.rb
@@ -1,37 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
 
-# Create some restaurants
-restaurant1 = Restaurant.create(name: "Pizza Hut")
-restaurant2 = Restaurant.create(name: "Domino's Pizza")
-restaurant3 = Restaurant.create(name: "Papa John's Pizza")
-restaurant4 = Restaurant.create(name: "Little Caesars")
-restaurant5 = Restaurant.create(name: "California Pizza Kitchen")
-restaurant6 = Restaurant.create(name: "Round Table Pizza")
+# Create 10 restaurants
+10.times do
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address
+  )
+end
 
-# Create some pizzas
-pizza1 = Pizza.create(name: "Margherita")
-pizza2 = Pizza.create(name: "Pepperoni")
-pizza3 = Pizza.create(name: "Vegetarian")
-pizza4 = Pizza.create(name: "Hawaiian")
-pizza5 = Pizza.create(name: "Meat Lovers")
-pizza6 = Pizza.create(name: "BBQ Chicken")
+# Create 20 pizzas
+20.times do
+  Pizza.create!(
+    name: Faker::Food.dish,
+    ingredients: Faker::Food.ingredient
+  )
+end
 
-# Associate pizzas with restaurants through restaurant_pizzas
-RestaurantPizza.create(restaurant: restaurant1, pizza: pizza1, price: 10)
-RestaurantPizza.create(restaurant: restaurant1, pizza: pizza2, price: 12)
-RestaurantPizza.create(restaurant: restaurant2, pizza: pizza2, price: 11)
-RestaurantPizza.create(restaurant: restaurant2, pizza: pizza3, price: 15)
-RestaurantPizza.create(restaurant: restaurant3, pizza: pizza1, price: 8)
-RestaurantPizza.create(restaurant: restaurant3, pizza: pizza3, price: 14)
-RestaurantPizza.create(restaurant: restaurant4, pizza: pizza3, price: 8)
-RestaurantPizza.create(restaurant: restaurant4, pizza: pizza4, price: 11)
-RestaurantPizza.create(restaurant: restaurant5, pizza: pizza2, price: 13)
-RestaurantPizza.create(restaurant: restaurant5, pizza: pizza6, price: 16)
-RestaurantPizza.create(restaurant: restaurant6, pizza: pizza1, price: 9)
-RestaurantPizza.create(restaurant: restaurant6, pizza: pizza5, price: 14)
+# Create 50 restaurant_pizzas
+50.times do
+  RestaurantPizza.create!(
+    price: rand(1...13),
+    restaurant: Restaurant.order("RANDOM()").first,
+    pizza: Pizza.order("RANDOM()").first
+  )
+end
